3854cba4f62b8884a1394ba93219e63a04d3bc74
XiaoZhou <598465994@qq.com>
2021-02-27 07:20:58 +0000

    注释Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,20 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+
+    /**
+     * Notifiable 是消息通知相关功能引用
+     * HasFactory 是模型工厂相关功能的引用
+     * Authenticatable 是授权相关功能的引用
+     */
     use HasFactory, Notifiable;
+
+    //数据库表
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
+     * 过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
      *
      * @var array
      */
@@ -24,6 +34,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
+     * 当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
      *
      * @var array
      */
