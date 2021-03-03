dba95f30e7581d80a1fe9a8c80d7fd37417e2675
XiaoZhou <598465994@qq.com>
2021-03-03 03:16:36 +0000

    更改用户资料

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,21 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 //注册
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+//下面几行同等上面一行代码
+// //显示所有用户列表的页面
+// Route::get('/users', 'UsersController@index')->name('users.index');
+// //显示用户个人信息的页面
+// Route::get('/users/create', 'UsersController@create')->name('users.create');
+// //创建用户的页面
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// //创建用户
+// Route::post('/users', 'UsersController@store')->name('users.store');
+// //编辑用户个人资料的页面
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// //更新用户
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+// //删除用户
+// Route::delate('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 
 //显示登录页面
 Route::get('login', 'SessionsController@create')->name('login');
