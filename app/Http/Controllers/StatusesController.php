38f0eb89b32ebea354e0ddf487e6a32d36a23a2e
XiaoZhou <598465994@qq.com>
2021-03-05 06:57:27 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -32,8 +32,18 @@ class StatusesController extends Controller
         return redirect()->back();
     }
 
-    public function destory()
+    /**
+     * 删除微博
+     * 这里我们使用的是『隐性路由模型绑定』功能，Laravel 会自动查找并注入对应 ID 的实例对象 $status，如果找不到就会抛出异常。
+     */
+    public function destroy(Status $status)
     {
-
+        //授权策略自动注册，App\Policies\StatusPolicy.php里面的destroy方法。。。做删除授权的检测，不通过会抛出 403 异常
+        $this->authorize('destroy', $status);
+        //调用 Eloquent 模型的 delete 方法对该微博进行删除
+        $status->delete();
+        session()->flash('success', '微博已被删除成功！');
+        //删除成功之后，将返回到执行删除微博操作的页面上。
+        return redirect()->back();
     }
 }
